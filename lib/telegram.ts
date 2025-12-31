5820fb52ce6eaa16bc0dbe25693260b3b8680385
jeongdongnyeok <dongnyeok@me.com>
2025-12-31 15:43:53 +0900

    Update telegram.ts

--- lib/telegram.ts
+++ lib/telegram.ts
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export async function sendTelegram(message: string) {
   const token = process.env.TG_TOKEN;
   const chatId = process.env.TG_CHAT_ID;
@@ -6,13 +8,8 @@ export async function sendTelegram(message: string) {
     return;
   }
   const url = `https://api.telegram.org/bot${token}/sendMessage`;
-
-  await fetch(url, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      chat_id: chatId,
-      text: message,
-    }),
+  await axios.post(url, {
+    chat_id: chatId,
+    text: message,
   });
 }
